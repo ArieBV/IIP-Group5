b3d186fca78dfe8861a2c35d4c76389b36d37cf6
Bodi <33829392+ArieBV@users.noreply.github.com>
2020-05-20 12:23:25 +0200

    Create 1_Connection_Test.ino

---
+++ Circuit_Playground_Express/1_Connection_Test/1_Connection_Test.ino
@@ -0,0 +1,27 @@
+#include <Adafruit_CircuitPlayground.h>
+#include <Adafruit_Circuit_Playground.h>
+
+//*********************************************
+// Time-Series Signal Processing
+// Rong-Hao Liang: r.liang@tue.nl
+//*********************************************
+
+
+int sampleRate = 100; //samples per second
+int sampleInterval = 1000000/sampleRate; //Inverse of SampleRate
+int data = 0;
+long timer = micros(); //timer
+
+void setup() {
+  Serial.begin(115200); //serial
+  CircuitPlayground.begin();
+}
+
+void loop() {
+  if (micros() - timer >= sampleInterval) { //Timer: send sensor data in every 10ms
+    timer = micros();
+    //data = analogRead(A8); //get the analog reading
+    data = CircuitPlayground.motionX();
+    Serial.println(data);
+  }
+}
